4ee724e9d4718d73382b06e0c2eb1a762dbf11df
Elias288 <bianchi.elias@gmail.com>
2026-03-10 15:01:47 -0300

    feat(Frontend): Listar usuarios
    Administradores pueden listar todos los usuarios, profesores solo alumnos
    
    close #1

--- frontend/src/pages/Usuarios/Usuarios.page.tsx
+++ frontend/src/pages/Usuarios/Usuarios.page.tsx
@@ -1,27 +1,70 @@
+import api from '@/api/client';
+import RequireRole from '@/components/RequireRole';
 import Search from '@/components/Search';
 import CardStyled from '@/components/styles/Card.style';
 import LinkButtonStyled from '@/components/styles/LinkButton.style';
-import { useAuth } from '@/context/AuthContext';
+import type { User } from '@/types/User.type';
+import { useEffect, useState } from 'react';
 
 type Props = {};
 const UsuariosPage = ({}: Props) => {
-  const { user } = useAuth();
+  const [usuarios, setUsuarios] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const getUsuarios = async () => {
+      try {
+        const res = await api.get<User[]>('/user');
+        setUsuarios(res.data);
+      } catch (error: any) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getUsuarios();
+  }, []);
+
+  if (loading)
+    return (
+      <div>
+        <CardStyled>
+          <h2>Cargando...</h2>
+        </CardStyled>
+      </div>
+    );
+  if (error)
+    return (
+      <div>
+        <CardStyled>
+          <h2>Error al cargar usuarios</h2>
+          <p>{error}</p>
+        </CardStyled>
+      </div>
+    );
 
   return (
     <div>
       <div className="wall__top flex flex-wrap gap-x-5 gap-y-3 pb-5 mb-5 border-b-2 border-gray-200">
-        {user?.role && user.role.search(/[^(ADMIN|PROFESOR)$]/g) && (
+        <RequireRole allowedRoles={['ADMIN', 'PROFESOR']}>
           <LinkButtonStyled title="Crear Usuario" to="/createUser" />
-        )}
+        </RequireRole>
         <Search />
       </div>
+
       <article className="wall__content flex flex-col gap-y-4">
-        <CardStyled>
-          <h2>Usuario1</h2>
-        </CardStyled>
-        <CardStyled>
-          <h2>Usuario2</h2>
-        </CardStyled>
+        {usuarios?.length > 0 &&
+          usuarios.map((u, key) => (
+            <CardStyled key={key}>
+              <h2 className="text-xl font-bold">
+                {u && u.name && u.name?.at(0)?.toUpperCase() + u.name?.slice(1)}
+              </h2>
+              <p>{u.email}</p>
+              <small>{u.role}</small>
+            </CardStyled>
+          ))}
       </article>
     </div>
   );
